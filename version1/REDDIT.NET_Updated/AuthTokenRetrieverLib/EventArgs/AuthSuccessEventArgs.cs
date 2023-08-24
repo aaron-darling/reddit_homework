55456dd1d1a10160a5bca6acf68bf45ec4c2c065
Aaron Darling <aaron.darling@darlingit.com>
2023-08-24 09:46:12 -0500

    cleanup

--- version1/REDDIT.NET_Updated/AuthTokenRetrieverLib/EventArgs/AuthSuccessEventArgs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Reddit.AuthTokenRetriever.EventArgs
-{
-    public class AuthSuccessEventArgs
-    {
-        public string AccessToken { get; set; }
-        public string RefreshToken { get; set; }
-    }
-}
